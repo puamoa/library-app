659896aa27e3264425d2050ee5abf8e215000197
puamoa <dp00615@gmail.com>
2024-12-23 21:05:19 +0900

    첫 번째 커밋

---
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -0,0 +1,86 @@
+package com.group.libraryapp.domain.user;
+
+//public class User {
+//  private String name;
+//
+//  private Integer age;
+//
+//  public User(String name, Integer age) {
+//    if (name == null || name.isBlank()) {
+//      throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다", name));
+//    }
+//    this.name = name;
+//    this.age = age;
+//  }
+//
+//  public String getName() {
+//    return name;
+//  }
+//
+//  public Integer getAge() {
+//    return age;
+//  }
+//
+//}
+//
+
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id = null;
+  @Column(nullable = false, length = 20)
+  private String name;
+  private Integer age;
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<UserLoanHistory> userLoanHistories = new ArrayList<>();
+
+  protected User() {  }
+
+  public User(String name, Integer age) {
+    if (name == null || name.isBlank()) {
+      throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다", name));
+    }
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void updateName(String name) {
+    this.name = name;
+  }
+
+  public void removeOneHistory() {
+    userLoanHistories.removeIf(history -> "책1".equals(history.getBookNamne()));
+  }
+
+  public void loanBook(String bookNamne) {
+    this.userLoanHistories.add(new UserLoanHistory(this, bookNamne));
+  }
+
+  public void returnBook(String bookNamne) {
+    UserLoanHistory targetHistory = this.userLoanHistories.stream()
+        .filter(history -> history.getBookNamne().equals(bookNamne))
+        .findFirst()
+        .orElseThrow(IllegalArgumentException::new);
+    targetHistory.doReturn();
+  }
+}
---
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -0,0 +1,69 @@
+package com.group.libraryapp.service.user;
+
+import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.domain.user.UserRepository;
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceV2 {
+  private final UserRepository userRepository;
+
+  public UserServiceV2(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Transactional
+  public void saveUsers() {
+    userRepository.save(new User("A", 10));
+    userRepository.save(new User("B", 20));
+    userRepository.save(new User("C", 30));
+  }
+
+  @Transactional(readOnly = true)
+  public void findUsers() {
+    User user1 = userRepository.findById(1L).get();
+    User user2 = userRepository.findById(1L).get();
+    User user3 = userRepository.findById(1L).get();
+  }
+
+  @Transactional
+  public void saveUser(UserCreateRequest request) {
+    userRepository.save(new User(request.getName(), request.getAge()));
+    throw new IllegalArgumentException();
+  }
+
+  @Transactional(readOnly = true)
+  public List<UserResponse> getUsers() {
+    return userRepository.findAll().stream()
+        .map(UserResponse::new)
+        .collect(Collectors.toList());
+  }
+
+
+  @Transactional
+  public void updateUser(UserUpdateRequest request) {
+    User user = userRepository.findById(request.getId())
+        .orElseThrow(IllegalArgumentException::new);
+
+    user.updateName(request.getName());
+  }
+
+
+  @Transactional
+  public void deleteUser(String name) {
+    User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
+    userRepository.delete(user);
+  }
+
+
+}
+
+
